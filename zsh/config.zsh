7c08b64765ae06b063dbe9573b1fa46b723b04a0
br41b4nt <alexis.braibant@gmail.com>
2026-01-11 17:14:52 +0100

    reorder .zshrc to register fzf

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -3,14 +3,15 @@ export HISTSIZE=1000000000
 export SAVEHIST=$HISTSIZE
 setopt EXTENDED_HISTORY
 
-# Enable fzf search in history with Ctrl+R
-source <(fzf --zsh)
 
 setopt autocd
 autoload -U compinit; compinit
 
 # HomeBrew
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
+
+# Enable fzf search in history with Ctrl+R
+source <(fzf --zsh)
 
 # Starship configuration
 export STARSHIP_CONFIG=~/.dotfiles/zsh/starship.toml
